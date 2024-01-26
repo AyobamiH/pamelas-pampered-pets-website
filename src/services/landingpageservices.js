f76fb6d8e7d79d58921fb6e1d8ff55ecbac673c4
AyobamiH <heryourbarme@live.com>
2024-01-26 10:30:33 +0000

    Add service functions for API calls
    Implemented functions in bookingService.js to fetch services, testimonials, and styles data from the API endpoints at localhost:3001/services, localhost:3001/testimonials, and localhost:3001/styles respectively, using axios for asynchronous HTTP requests.

---
+++ src/services/landingpageservices.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001';
+
+const getServices = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/services`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const getTestimonials = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/testimonials`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+  const getStyles = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/styles`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+
+
+export default { getServices, getTestimonials, getStyles };
